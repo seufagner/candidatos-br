1589b2bfff0429197a007bac12165e9ef767e182
Fagner Moura <fagnermoura@gmail.com>
2016-05-09 16:37:40 -0300

    adicionando layout candidato 2010 e 2012

--- info_candidatos.R
+++ info_candidatos.R
@@ -5,6 +5,7 @@ cand_path <- "raw_data/consulta_cand_2014/"
 leg_path <- "raw_data/consulta_legendas_2014/"
 
 bens_df <- load_files_on(bens_cand_path, with_col_names = cols_bem_candidato)
-candidatos_df <- load_files_on(cand_path, with_col_names = cols_consulta_candidato_2014)
+candidatos_df <- load_files_on(cand_path, with_col_names = cols_consulta_candidato_2012_2014)
 legendas_df <- load_files_on(leg_path, with_col_names = cols_legenda)
 
+names(bens_df)
