687ad7dd25ce47c786729f2169e7d58792053573
Fagner Moura <fagnermoura@gmail.com>
2016-05-10 13:48:30 -0300

    analise bens psol

--- info_candidatos.R
+++ info_candidatos.R
@@ -13,22 +13,34 @@ legendas_df <- load_files_on(leg_path, with_col_names = cols_legenda)
 candidatos_por_raca <- aggregate(candidatos_df$CODIGO_COR_RACA, by=list(candidatos_df$DESCRICAO_COR_RACA), FUN=sum)
 pie(candidatos_por_raca$x, labels = candidatos_por_raca$Group.1, main="Candidatos por raça 2014")
 
-simple_candidatos_df <- subset(candidatos_df, select = c("SEQUENCIAL_CANDIDATO", 
-                                                         "NOME_CANDIDATO",
-                                                         "NOME_PARTIDO", 
-                                                         "SIGLA_PARTIDO",
-                                                         "NUMERO_PARTIDO", 
-                                                         "CPF_CANDIDATO",
-                                                         "CODIGO_SEXO",
-                                                         "DESCRICAO_GRAU_INSTRUCAO",
-                                                         "COD_GRAU_INSTRUCAO",
-                                                         "CODIGO_CARGO",
-                                                         "DESCRICAO_CARGO",
-                                                         "DESPESA_MAX_CAMPANHA"))
-
-
-x <- simple_candidatos_df %>%
+x <- candidatos_df %>%
   group_by(SIGLA_PARTIDO) %>%
   summarise(total = sum(DESPESA_MAX_CAMPANHA),freq = n())
 pie(x$total, labels = x$SIGLA_PARTIDO, main="Gasto campanha 2014")
+
+candidato_partido <- candidatos_df %>%
+  select(SEQUENCIAL_CANDIDATO, NOME_CANDIDATO, NUMERO_PARTIDO, SIGLA_PARTIDO, ANO_ELEICAO, SIGLA_UF, SIGLA_UE, CODIGO_CARGO, DESCRICAO_CARGO)
+  
+candidato_bens_total <- bens_df %>% 
+  group_by(SQ_CANDIDATO) %>%
+  summarise(total = sum(VALOR_BEM),freq = n())
+
+candidato_bens <- merge(candidato_partido, candidato_bens_total, by.x = "SEQUENCIAL_CANDIDATO", by.y = "SQ_CANDIDATO")
+
+partido_bens <- candidato_bens %>%
+  group_by(SIGLA_PARTIDO) %>%
+  summarise(TOTAL_PARTIDO = sum(total),freq = n())
+pie(partido_bens$TOTAL_PARTIDO, partido_bens$SIGLA_PARTIDO, mmain = "Total bens por partido 2014")
+
+x <- candidato_bens %>%
+  group_by(SIGLA_UF) %>%
+  filter(SIGLA_PARTIDO == "PSOL") %>%
+  summarise(TOTAL_UF = sum(total), freq = n())
+pie(x$TOTAL_UF, x$SIGLA_UF, main = "Total bens PSOL por Estado 2014")
+
+summary(candidato_bens$total)
+candidato_bens %>% 
+  select(SIGLA_UF, NOME_CANDIDATO, DESCRICAO_CARGO, SIGLA_PARTIDO, total) %>%
+  filter(total == 2135000000)
+
 
