d324dadd2888acd472f61744ae482b74f4c7d405
Fagner Moura <fagnermoura@gmail.com>
2016-05-10 15:14:52 -0300

    fix layout do arquivo consulta_candidato_xxxx

--- info_candidatos.R
+++ info_candidatos.R
@@ -2,13 +2,19 @@ library(dplyr)
 library(plyr)
 source("tse_file_loader.R")
 
-bens_cand_path <- "raw_data/bem_candidato_2014/"
-cand_path <- "raw_data/consulta_cand_2014/"
-leg_path <- "raw_data/consulta_legendas_2014/"
+bens_cand_2012_path <- "raw_data/bem_candidato_2012/"
+cand_2012_path <- "raw_data/consulta_cand_2012/"
+leg_2012_path <- "raw_data/consulta_legendas_2012/"
+bens_cand_2014_path <- "raw_data/bem_candidato_2014/"
+cand_2014_path <- "raw_data/consulta_cand_2014/"
+leg_2014_path <- "raw_data/consulta_legendas_2014/"
 
-bens_df <- load_files_on(bens_cand_path, with_col_names = cols_bem_candidato)
-candidatos_df <- load_files_on(cand_path, with_col_names = cols_consulta_candidato_2012_2014)
-legendas_df <- load_files_on(leg_path, with_col_names = cols_legenda)
+bens_2012_df <- load_files_on(bens_cand_2012_path, with_col_names = cols_bem_candidato)
+candidatos_2012_df <- load_files_on(cand_2012_path, with_col_names = cols_consulta_candidato_2012)
+legendas_2012_df <- load_files_on(leg_2012_path, with_col_names = cols_legenda)
+bens_2014_df <- load_files_on(bens_cand_2014_path, with_col_names = cols_bem_candidato)
+candidatos_2014_df <- load_files_on(cand_2014_path, with_col_names = cols_consulta_candidato_2014)
+legendas_2014_df <- load_files_on(leg_2014_path, with_col_names = cols_legenda)
 
 candidatos_por_raca <- aggregate(candidatos_df$CODIGO_COR_RACA, by=list(candidatos_df$DESCRICAO_COR_RACA), FUN=sum)
 pie(candidatos_por_raca$x, labels = candidatos_por_raca$Group.1, main="Candidatos por raça 2014")
@@ -32,15 +38,31 @@ partido_bens <- candidato_bens %>%
   summarise(TOTAL_PARTIDO = sum(total),freq = n())
 pie(partido_bens$TOTAL_PARTIDO, partido_bens$SIGLA_PARTIDO, mmain = "Total bens por partido 2014")
 
+###
 x <- candidato_bens %>%
   group_by(SIGLA_UF) %>%
   filter(SIGLA_PARTIDO == "PSOL") %>%
   summarise(TOTAL_UF = sum(total), freq = n())
 pie(x$TOTAL_UF, x$SIGLA_UF, main = "Total bens PSOL por Estado 2014")
 
-summary(candidato_bens$total)
 candidato_bens %>% 
   select(SIGLA_UF, NOME_CANDIDATO, DESCRICAO_CARGO, SIGLA_PARTIDO, total) %>%
   filter(total == 2135000000)
 
+###
 
+pmdb <- candidato_bens %>%
+  group_by(SIGLA_UF) %>%
+  filter(SIGLA_PARTIDO == "PMDB") %>%
+  summarise(TOTAL_UF = sum(total), freq = n())%>%
+  arrange(TOTAL_UF)
+pie(pmdb$TOTAL_UF, pmdb$SIGLA_UF, main = "Total bens PMDB por estado 2014")
+
+###
+
+psdb <- candidato_bens %>%
+  group_by(SIGLA_UF) %>%
+  filter(SIGLA_PARTIDO == "PSDB") %>%
+  summarise(TOTAL_UF = sum(total), freq = n())%>%
+  arrange(TOTAL_UF)
+pie(pmdb$TOTAL_UF, pmdb$SIGLA_UF, main = "Total bens PSDB por estado 2014")
--- tse_file_loader.R
+++ tse_file_loader.R
@@ -24,7 +24,7 @@ cols_bem_candidato <- c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO",
                         "DETALHE_BEM","VALOR_BEM","DATA_ULTIMA_ATUALIZACAO",
                         "HORA_ULTIMA_ATUALIZACAO")
 
-cols_consulta_candidato_2012_2014 <- c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO",
+cols_consulta_candidato_2014 <- c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO",
                                   "NUM_TURNO", "DESCRICAO_ELEICAO","SIGLA_UF",
                                   "SIGLA_UE","DESCRICAO_UE","CODIGO_CARGO",
                                   "DESCRICAO_CARGO", "NOME_CANDIDATO", 
@@ -43,6 +43,25 @@ cols_consulta_candidato_2012_2014 <- c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEI
                                   "SIGLA_UF_NASCIMENTO","CODIGO_MUNICIPIO_NASCIMENTO",
                                   "NOME_MUNICIPIO_NASCIMENTO","DESPESA_MAX_CAMPANHA",
                                   "COD_SIT_TOT_TURNO","DESC_SIT_TOT_TURNO", "NM_EMAIL")
+
+cols_consulta_candidato_2012 <-  c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO",
+                                   "NUM_TURNO", "DESCRICAO_ELEICAO","SIGLA_UF",
+                                   "SIGLA_UE","DESCRICAO_UE","CODIGO_CARGO",
+                                   "DESCRICAO_CARGO", "NOME_CANDIDATO", 
+                                   "SEQUENCIAL_CANDIDATO","NUMERO_CANDIDATO", 
+                                   "CPF_CANDIDATO","NOME_URNA_CANDIDATO", 
+                                   "COD_SITUACAO_CANDIDATURA", "DES_SITUACAO_CANDIDATURA",
+                                   "NUMERO_PARTIDO", "SIGLA_PARTIDO","NOME_PARTIDO",
+                                   "CODIGO_LEGENDA","SIGLA_LEGENDA","COMPOSICAO_LEGENDA",
+                                   "NOME_LEGENDA","CODIGO_OCUPACAO","DESCRICAO_OCUPACAO",
+                                   "DATA_NASCIMENTO","NUM_TITULO_ELEITORAL_CANDIDATO",
+                                   "IDADE_DATA_ELEICAO","CODIGO_SEXO","DESCRICAO_SEXO",
+                                   "COD_GRAU_INSTRUCAO","DESCRICAO_GRAU_INSTRUCAO",
+                                   "CODIGO_ESTADO_CIVIL","DESCRICAO_ESTADO_CIVIL",
+                                   "CODIGO_NACIONALIDADE","DESCRICAO_NACIONALIDADE",
+                                   "SIGLA_UF_NASCIMENTO","CODIGO_MUNICIPIO_NASCIMENTO",
+                                   "NOME_MUNICIPIO_NASCIMENTO","DESPESA_MAX_CAMPANHA",
+                                   "COD_SIT_TOT_TURNO","DESC_SIT_TOT_TURNO", "NM_EMAIL")
 
 cols_consulta_candidato_ate_2010 <- c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO",
                                        "NUM_TURNO", "DESCRICAO_ELEICAO","SIGLA_UF",
